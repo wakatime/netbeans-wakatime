d74833973911de2d7adc5332b0d0756d0d8fe028
Alan Hamlett <alan.hamlett@gmail.com>
2014-12-22 01:15:59 -0600

    upgrade wakatime-cli to v2.1.11

--- src/org/wakatime/netbeans/plugin/Dependencies.java
+++ src/org/wakatime/netbeans/plugin/Dependencies.java
@@ -21,7 +21,7 @@ public class Dependencies {
 
     private static String pythonLocation = null;
     private static String resourcesLocation = null;
-    private static final String cliVersion = "2.1.10";
+    private static final String cliVersion = "2.1.11";
 
     public static boolean isPythonInstalled() {
         return Dependencies.getPythonLocation() != null;
